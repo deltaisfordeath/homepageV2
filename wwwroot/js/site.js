042c3761e2317a08799bc18750b312cf63985c6f
Kenny Heagle <kennyheagle@gmail.com>
2025-01-11 17:40:34 -0700

    add styling and animate nav collapse

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -8,6 +8,12 @@ menuButton.addEventListener('click', () => {
     const isExpanded = menuButton.getAttribute('aria-expanded') === 'true';
     menuButton.setAttribute('aria-expanded', !isExpanded);
     
+    if (isExpanded) {
+        mobileMenu.style.height = '0px';
+    } else {
+        mobileMenu.style.height = mobileMenu.scrollHeight + 'px';
+    }
+    
     menuIcons.forEach((icon) => {
         icon.classList.toggle('hidden');
         icon.classList.toggle('block');
